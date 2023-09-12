4ff0ec8d913e5a2e9b4e69bc361bb7adb4416000
Dmitriy Korlykhanov <dimakorla@gmail.ru>
2023-09-12 22:11:36 +0300

    Exercises done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date, timedelta
 """
 Домашнее задание №2
 
@@ -9,19 +10,19 @@
 """
 
 def print_days():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    dt_now = date.today()
+    delta = timedelta(days=1)
+    dt_yesterday = dt_now - delta
+    delta = timedelta(days=30)
+    dt_30 = dt_now - delta
+    print(f"Вчера: {dt_yesterday}")
+    print(f"Сегодня: {dt_now}")
+    print(f"30 дней назад: {dt_30}")
 
 
 def str_2_datetime(date_string):
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    date_dt = datetime.strptime(date_string, '%m/%d/%y %H:%M:%S.%f')
+    return date_dt
 
 if __name__ == "__main__":
     print_days()
--- 3_dict_to_csv.py
+++ 3_dict_to_csv.py
@@ -1,3 +1,4 @@
+import csv
 """
 
 Домашнее задание №2
@@ -11,11 +12,19 @@
 """
 
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+    users= [
+            {'name': 'Маша', 'age': 25, 'job': 'Scientist'}, 
+            {'name': 'Вася', 'age': 18, 'job': 'Programmer'}, 
+            {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+            {'name': 'Миша', 'age': 23, 'job': 'Programmer'},
+            {'name': 'Женя', 'age': 34, 'job': 'Scientist'},
+        ]
+    with open('export.csv', 'w', encoding='windows-1251', newline='') as f:
+        fields = ['name', 'age', 'job']
+        writer = csv.DictWriter(f, fields, delimiter=';')
+        writer.writeheader()
+        for user in users:
+            writer.writerow(user)
 
 if __name__ == "__main__":
     main()
